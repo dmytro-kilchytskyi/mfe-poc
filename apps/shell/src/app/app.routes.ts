58497342d6be7e2a802d0931377af0b42eb5a38f
Dmytro Kilchytskyi <d.kilchytskyi@gmail.com>
2025-10-01 12:44:42 +0200

    chore(shell): cleanup

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'remote2',
-    loadChildren: () => import('remote2/Routes').then((m) => m!.remoteRoutes),
-  },
-  {
     path: 'remote1',
     loadChildren: () => import('remote1/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
+    path: 'remote2',
+    loadChildren: () => import('remote2/Routes').then((m) => m!.remoteRoutes),
   },
   {
     path: '',
