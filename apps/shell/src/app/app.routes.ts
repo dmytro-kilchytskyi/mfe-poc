f4d307a600ea9c8c5b40ee02e61cb488857e40b5
Dmytro Kilchytskyi <d.kilchytskyi@gmail.com>
2025-11-03 22:23:18 +0100

    setup(remotes): update production webpack config

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -2,11 +2,6 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'cart',
-  },
-  {
     path: 'cart',
     loadChildren: () => import('cart/Routes').then((m) => m!.remoteRoutes),
   },
--- apps/shell/webpack.prod.config.ts
+++ apps/shell/webpack.prod.config.ts
@@ -12,13 +12,11 @@ export default withModuleFederation(
     /*
      * Remote overrides for production.
      * Each entry is a pair of a unique name and the URL where it is deployed.
-     *
-     * e.g.
-     * remotes: [
-     *   ['app1', 'https://app1.example.com'],
-     *   ['app2', 'https://app2.example.com'],
-     * ]
      */
+    remotes: [
+      ['products', 'https://mfe-poc-cdn.cloudstats.org/products'],
+      ['cart', 'https://mfe-poc-cdn.cloudstats.org/cart'],
+    ],
   },
   { dts: false }
 );
